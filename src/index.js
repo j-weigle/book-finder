20bdfc8d7e202596db733be0d5f539d7712aed1b
Justin Weigle <justin.weigle2@gmail.com>
2021-05-02 02:16:45 -0400

    api call working and displaying just titles for now

--- src/index.js
+++ src/index.js
@@ -2,22 +2,90 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 
+function parseRawBooksJson (rawJson) {
+  const { items } = rawJson;
+  if (!items) return [];
+
+  return items.map(bookItem => {
+    const volumeInfo = bookItem.volumeInfo;
+    const saleInfo = bookItem.saleInfo;
+
+    const {
+      authors,
+      averageRating,
+      categories,
+      description,
+      infoLink,
+      pageCount,
+      previewLink,
+      publishedDate,
+      publisher,
+      subtitle,
+      title
+    } = volumeInfo;
+    const bookCover = volumeInfo.imageLinks ? volumeInfo.imageLinks.thumbnail : 'https://www.lse.ac.uk/International-History/Images/Books/NoBookCover.png';
+
+    return {
+      authors,
+      description,
+      bookCover,
+      infoLink,
+      pageCount,
+      previewLink,
+      publishedDate,
+      publisher,
+      subtitle,
+      title
+    };
+  });
+}
+
+async function fetchBooksFromAPI (query) {
+  const apiURL = 'https://www.googleapis.com/books/v1/volumes?q=' + query;
+  try {
+    const res = await fetch(apiURL);
+    const rawJson = await res.json();
+    const bookList = await parseRawBooksJson(rawJson);
+    return bookList;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
 class Result extends React.Component {
+  render () {
+    const { title } = this.props;
+    return (
+      <div className="result">
+        <div className="result_title">
+          <h3>{title}</h3>
+        </div>
+      </div>
+    );
+  }
 }
 
 class Results extends React.Component {
   render () {
-    return( // will return an array of Result objects (Books)
-      <div class="results-area">
-      </div>
-    );
+    if (this.props.bookList.length > 0) {
+      return this.props.bookList.map((book, idx) => (
+          <Result 
+            key={idx}
+            title={book.title}
+          />
+      ));
+    } else {
+      return (
+        <div className="empty-results-area">No results to show...</div>
+      );
+    }
   }
 }
 
 class Search extends React.Component {
   render () {
     return(
-      <div class="search-area">
+      <div className="search-area">
         <input
           onChange={this.props.onChange}
         />
@@ -31,7 +99,8 @@ class Base extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      query: ''
+      query: '',
+      results: []
     };
   }
 
@@ -44,23 +113,26 @@ class Base extends React.Component {
   handleClick () {
     const { query } = this.state;
     if (!query) return;
-    alert('user clicked submit with query: ' + query);
+    fetchBooksFromAPI(query)
+      .then(results => {
+        this.setState({results});
+      });
   }
 
   render () {
     return (
-      <div class="wrapper">
+      <div className="wrapper">
         <header>
           <h1>Book Finder</h1>
         </header>
-        <div class="search-wrapper">
+        <div className="search-wrapper">
           <Search 
             onChange={(userIn) => this.handleChange(userIn)}
             onClick={() => this.handleClick()}
           />
         </div>
-        <div class="results-wrapper">
-          <Results />
+        <div className="results-wrapper">
+          <Results bookList={this.state.results} />
         </div>
       </div>
     );
