015c43269b9ebed2c91d679494b6936c9cc7ab1d
Justin Weigle <justin.weigle2@gmail.com>
2021-05-16 21:37:56 -0400

    show loading animation in results area when search is performed

--- src/js/books.js
+++ src/js/books.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Book } from './book';
+import loadingGIF from '../img/loading.gif';
 
 export class Books extends React.Component {
   render () {
-    if (this.props.bookList.length > 0) {
+    if (this.props.loading) {
+      return (
+        <div className="loading-wrapper">
+          <img className="loading-gif" src={loadingGIF} alt=""/>
+          <p>Fetching results...</p>
+        </div>
+      );
+    } else if (this.props.bookList.length > 0) {
       return this.props.bookList.map((book, idx) => (
           <Book
             key={idx}
